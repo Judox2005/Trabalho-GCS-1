50502885c54c5ec945ab0993ab6c7751acfa343a
ArthurFaleiro <164569976+ArthurFaleiro@users.noreply.github.com>
2024-09-27 21:20:32 +0000

    Cria a classe Pedido

--- src/Pedido.java
+++ src/Pedido.java
@@ -3,6 +3,14 @@ public class Pedido{
     private String dataInicio;
     private String dataConclusao;
     private double valorTotal;
+    private int id;
+    public Pedido(String status, String dataInicio, String dataConclusao, double valorTotal, int id){
+        this.status = status;
+        this.dataInicio = dataInicio;
+        this.dataConclusao = dataConclusao;
+        this.valorTotal = valorTotal;
+        this.id = id;
+    }
     
 
 }
