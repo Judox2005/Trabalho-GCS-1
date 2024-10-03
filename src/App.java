69b30c2c512d232e832832aeb10eaef0398c8604
Piedro Rockembach <79763019+PiedroRockembach@users.noreply.github.com>
2024-10-03 20:59:20 -0300

    adiciona o atributo registro na app

--- src/App.java
+++ src/App.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
-
 public class App {
     private Usuario usuario;
     private ArrayList<Usuario> usuarios;
+    private Registro registro;
     public App(){
         this.usuario = null;
-        this.usuarios = new ArrayList();
+        this.usuarios = new ArrayList<>();
+        this.registro = new Registro();
 
     }
 
     public void executar(){
-        
     }
     public void menu(){
 
