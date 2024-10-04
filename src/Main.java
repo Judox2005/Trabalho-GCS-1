7b1614f873df0b4a5cb8df53961bdae892af7886
ArthurFaleiro <164569976+ArthurFaleiro@users.noreply.github.com>
2024-10-04 01:41:41 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,12 @@ public class Main{
         App app = new App();
         app.executar();
     }
+ private static List<Departamento> departamentos = new ArrayList<>();
+ private static void iniciarDepartamento(){
+     departamentos.add(new Departamento("Financeiro",1000.0));
+     departamentos.add(new Departamento("TI",4000.0));
+     departamentos.add(new Departamento("RH",2000.0));
+     departamentos.add(new Departamento("Engenharia",1000.0));
+     departamentos.add(new Departamento("Manutenção",600.0));    
+ }   
 }
