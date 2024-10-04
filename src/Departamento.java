81cc6843a119d93688a3246a7da86de61fd3efbe
ArthurFaleiro <164569976+ArthurFaleiro@users.noreply.github.com>
2024-10-04 01:36:32 -0300

    Update Departamento.java

--- src/Departamento.java
+++ src/Departamento.java
@@ -1,13 +1,20 @@
 import java.util.ArrayList;
 
 public class Departamento {
-    private int valorMaximoPedido;
+    private double valorMaximoPedido;
     private String nome;
     private ArrayList<Usuario> membrosDepartamento;
 
-    public Departamento(int valorMaximoPedido, String nome) {
+    public Departamento(double valorMaximoPedido, String nome) {
         this.valorMaximoPedido = valorMaximoPedido;
         this.nome = nome;
         this.membrosDepartamento = new ArrayList<>();
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public double getvalorMaximoPedido(){
+        return valorMaximoPedido;
     }
 }
